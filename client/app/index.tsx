6a6b36a9108081f8f021b8ebcfbe354759c632ae
Setsudan <cryptiareta@gmail.com>
2023-10-13 16:57:57 +0200

    🚨 : Linter added

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Welcome to my React Native app!</Text>
+        </View>
+    );
+};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
 });
+
+export default App;
