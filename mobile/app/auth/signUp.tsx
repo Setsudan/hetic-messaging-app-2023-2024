166920fd2867734b626d41ef35b78bde258bd294
Setsudan <cryptiareta@gmail.com>
2023-10-24 09:42:16 +0200

    🚧 : SignUp screen in progress

--- mobile/app/auth/signUp.tsx
+++ mobile/app/auth/signUp.tsx
@@ -1,34 +1,96 @@
-import { Layout, Text } from "@ui-kitten/components";
-import { useEffect, useState } from "react";
+import { Layout, Text, Input, Button } from "@ui-kitten/components"
+import { SignUpBody } from "../../types/API/auth.types"
+import { useState } from "react"
+import { View } from "react-native"
 
-// const socket = new WebSocket('ws://192.168.1.221:8080');
-const socket = new WebSocket(`ws://${process.env.EXPO_PUBLIC_API_IP}:8080`);
+const emailRegex = new RegExp('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$');
+const passwordRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])');
 
 export default function SignUpScreen() {
 
-    useEffect(() => {
-        console.log('Connecting...');
-        // Handle messages received from the server
-        socket.onmessage = (event) => {
-            console.log('Received message from server:', event.data);
-        };
+    const [signUpBody, setSignUpBody] = useState<SignUpBody>({
+        display_name: "",
+        email: "",
+        password: "",
+        username: "",
+    })
 
-        // Send a message to the server
-        socket.send('Hello, server!');
+    const [confirmPassword, setConfirmPassword] = useState<string>("")
 
-        return () => {
-            // Close the WebSocket connection when the component unmounts
-            socket.close();
-        };
-    }, []);
+    const [errors, setErrors] = useState<string[]>([])
 
+    const handleSubmission = () => {
+        if (!emailRegex.test(signUpBody.email)) {
+            setErrors([...errors, "Invalid email"])
+        }
+        if (!passwordRegex.test(signUpBody.password)) {
+            setErrors([...errors, "Invalid password"])
+        }
+        if (signUpBody.password !== confirmPassword) {
+            setErrors([...errors, "Passwords do not match"])
+        }
+
+        if (errors.length > 0) {
+            console.log('Signup body is invalid')
+            console.log(signUpBody)
+            console.log(errors)
+            return
+        }
+
+        console.log('Signup body is valid')
+        console.log(signUpBody)
+    }
 
     return (
         <Layout style={{ flex: 1 }}>
             <Text category="h1" style={{ textAlign: "center", marginTop: 32 }}>
-                SignUp screen
+                Sign Up
             </Text>
-            {/* <Button onPress={() => { handlePress() }}>Sign Up</Button> */}
+            <View style={{ flex: 1, margin: 16, borderRadius: 18 }}>
+                <Text category="label">
+                    Username
+                </Text>
+                <Input
+                    value={signUpBody.username}
+                    onChangeText={(text) => setSignUpBody({ ...signUpBody, username: text })}
+                />
+                <Text category="label">
+                    Email
+                </Text>
+                <Input
+                    value={signUpBody.email}
+                    onChangeText={(text) => setSignUpBody({ ...signUpBody, email: text })}
+                />
+                <Text category="label">
+                    Display Name
+                </Text>
+                <Input
+                    value={signUpBody.display_name}
+                    onChangeText={(text) => setSignUpBody({ ...signUpBody, display_name: text })}
+                />
+                <Text category="label">
+                    Password
+                </Text>
+                <Input
+                    value={signUpBody.password}
+                    onChangeText={(text) => setSignUpBody({ ...signUpBody, password: text })}
+                />
+                <Text category="label">
+                    Comfirm Password
+                </Text>
+                <Input
+                    value={confirmPassword}
+                    onChangeText={(text) => setConfirmPassword(text)}
+                />
+            </View>
+            <Text category="label" style={{ textAlign: "center", color: "red" }}>
+                {errors.map((error) => <Text
+                    key={error}
+                >{error}</Text>)}
+            </Text>
+            <Button onPress={handleSubmission}>
+                Sign Up
+            </Button>
         </Layout>
     )
 }
